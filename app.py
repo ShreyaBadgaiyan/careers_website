a012ca88438f9320c504e8cbe9c632f839da9f26
Shreya Badgaiyan <117737111+ShreyaBadgaiyan@users.noreply.github.com>
2024-11-06 01:39:47 +0530

    frontend on progress

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 # print("Hello World")
 
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello Shreya Badgaiyan!"
+
+@app.route("/profile")
+def show_profile():
+     return render_template('index.html')
+     
+
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
